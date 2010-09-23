f3e6f14b6bbaf088e050b200bd7ec39776808479
nimishg <nimishg@410e43ee-5b00-4399-9fea-894f6e993c6a>
2010-09-23 20:57:04 +0000

    made mining scripts less ugly and output csvs

--- mine_impression_requests.php
+++ mine_impression_requests.php
@@ -1,44 +1,20 @@
 <?php error_reporting (E_NOTICE && E_ALL);
 
 /* 
-* mine_impression_requests.php <input_file> <date>
-* date is optional and should conform to the following format '2009-11-18'
+* mine_impression_requests.php <input_file>
 */
 
-if ( isset( $argv[2] ) ) {
-	$date_passed = date("Y-m-d", strtotime( $argv[2] ) );
+
+
+if ( preg_match( "/gz/", $argv[1] ) ) { 
+	$handle = gzopen($argv[1], 'r' ) or die ("Could not open file $argv[1]");	
 } else {
-	$date_passed = date("Y-m-d", strtotime("now"));
-}
-
-
-$file_path = '/a/squid/fundraising/logs/';
-$file_name = 'landingpages2010.log';
-
-if (!$argv[1]) {
-	$file_date = date( 'Ymd', strtotime( 'now' ) );
-	$full_path = $file_path . $file_name . $file_date;
-	print "Opening $full_path \n";
-	$handle = fopen( $full_path, 'r' ) or die ("Could not open file $full_path");
-} else {
-	if ( preg_match( "/gz/", $argv[1] ) ) { 
-		$handle = gzopen($argv[1], 'r' ) or die ("Could not open file $argv[1]");	
-	} else {
-		$handle = fopen($argv[1], 'r') or die ("Could not open file $argv[1]");
-	}	
-}
+	$handle = fopen($argv[1], 'r') or die ("Could not open file $argv[1]");
+}	
 
 $counts = array();
 
-print "Impressions for $date_passed\n";
-
 while(( $row = fgetcsv( $handle,'',' ') ) !== FALSE) {
- 	$datetime = $row[2];
-	$date = substr( $datetime, 0, 10 );
-
-	if ( strcmp( $date, $date_passed ) != 0 ) {
-		continue;
-	}	
 
 	$tracked_url = $row[8];
 	$parsed_url = parse_url( $tracked_url );
@@ -54,12 +30,24 @@ while(( $row = fgetcsv( $handle,'',' ') ) !== FALSE) {
 
 		if ( $project == '' ) {
 			// print "Missing DB: $target_url\n";
-			$counts[$banner]['NONE'];
+			$counts[$banner]['NONE']++;
 		} else {
 			$counts[$banner][$project]++;
 		}
 	}
 }
 
-print_r($counts);
+$stdout = fopen("php://stdout", "a");
+fputcsv($stdout, array ( 
+	"banner", "project", "counts",	
+	)
+);
+
+foreach($counts as $bannername => $project_array){
+	foreach ($project_array as $project => $count){
+		fputcsv($stdout, array( $bannername, $project, $count));
+	}
+}
+
+//print_r($counts);
 ?>
--- mine_landing_pages.php
+++ mine_landing_pages.php
@@ -1,46 +1,20 @@
 <?php error_reporting (E_NOTICE && E_ALL);
 
 /* 
- * mine_landing_pages.php <input_file> <date>
- * date is optional and should conform to the following format '2009-11-18'
+ * mine_landing_pages.php <input_file>
  */
-
-
-if ( isset( $argv[2] ) ) {
-	$date_passed = $argv[2];
+$handle = null;
+if ( preg_match( "/gz/", $argv[1] ) ) { 
+	$handle = gzopen($argv[1], 'r' ) or die ("Could not open file $argv[1]\n");	
 } else {
-	$date_passed = date("Y-m-d", strtotime("now"));
-}
-
-
-$file_path = '/a/squid/fundraising/logs/';
-$file_name = 'landingpages2010.log';
-
-if( !$argv[1] ) {
-	$file_date = date( 'Ymd', strtotime( 'now' ) );
-	$full_path = $file_path . $file_name . $file_date;
-	print "Opening $full_path \n";
-	$handle = fopen( $full_path, 'r' ) or die ("Could not open file $full_path\n");
-} else {
-	if ( preg_match( "/gz/", $argv[1] ) ) { 
-		$handle = gzopen($argv[1], 'r' ) or die ("Could not open file $argv[1]\n");	
-	} else {
-		$handle = fopen($argv[1], 'r') or die ("Could not open file $argv[1]\n");
-	}	
-}
+	$handle = fopen($argv[1], 'r') or die ("Could not open file $argv[1]\n");
+}	
 
 $counts = array();
 $counts_csv = '';
 
-print "Clicks for $date_passed\n";
 
 while(( $row = fgetcsv($handle,'',' ') ) !== FALSE) {
- 	$datetime = $row[2];
-	$date = substr( $datetime, 0, 10 );
-
-	if ( strcmp( $date, $date_passed ) != 0 ) {
-		continue;
-	}	
 
 	$referrer_url = $row[11];
 	$parsed_url = parse_url( $referrer_url );
@@ -80,20 +54,18 @@ while(( $row = fgetcsv($handle,'',' ') ) !== FALSE) {
 
 print_r($counts);
 
-
 //Quick hack to get it in
+
+$stdout = fopen("php://stdout", "a");
 
 $csv_yes = 1;
 if ( $csv_yes ) {	
-	//$fp = fopen('file.csv', 'w');
 	foreach( $counts as $utm_source => $project ) { 
 		foreach( $project as $project_p => $landing_page ) {
 			foreach( $landing_page as $landing_page_p => $amount ) { 
-				print $utm_source . ',' . $project_p . ',' . $landing_page_p . ',' . $amount . "\n";
+				fputcsv( $stdout, array($utm_source,  $project_p,  $landing_page_p ,  $amount ));
 			}
 		}
-	//	fputcsv( $fp, $counts );
 	}
-	//fclose($fp);
 }
 ?>
