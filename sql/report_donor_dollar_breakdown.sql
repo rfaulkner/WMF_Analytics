20b38d5bd5c6afcf2c5a77c41c35d36813cd5910
rfaulk <rfaulk@410e43ee-5b00-4399-9fea-894f6e993c6a>
2011-07-27 05:05:34 +0000

    fixed some faulty formatting, added comments

--- sql/report_donor_dollar_breakdown.sql
+++ sql/report_donor_dollar_breakdown.sql
@@ -1,4 +1,5 @@
-
+-- formatted SQL query for breaking donation counts into categories based on amount donated 
+-- used by DataLoader.DonorBracketsReportingLoader
 
 select
 
@@ -17,13 +18,11 @@ join
 
 (
 select
--- %s
-SUBSTRING_index(substring_index(utm_source, '.', 2),'.',1) as artifact,
+%s
 total_amount
 
 from drupal.contribution_tracking left join civicrm.civicrm_contribution on (contribution_tracking.contribution_id = civicrm.civicrm_contribution.id)
--- where receive_date > '%s' and receive_date <= '%s' and utm_campaign = '%s'
-where receive_date > '20110722181600' and receive_date <= '20110722192000' and utm_campaign = 'C_KA2LP_Julytest_US'
+where receive_date > '%s' and receive_date <= '%s' and utm_campaign = '%s'
 ) as civi
 
 
