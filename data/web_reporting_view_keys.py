79e0ee7677e09cebed4726a72c91d3c276b55cbb
rfaulk <rfaulk@410e43ee-5b00-4399-9fea-894f6e993c6a>
2011-11-19 03:33:33 +0000

    re: r823 -- modify: added caching to live results views

--- data/web_reporting_view_keys.py
+++ data/web_reporting_view_keys.py
@@ -1,2 +1,3 @@
 
 LTT_DICT_KEY = 'LTT_DICT_KEY' # View key for the long term trends viiew in web_reporting.live_results.views
+LIVE_RESULTS_DICT_KEY = 'LR_DICT_KEY' # View key for the long term trends viiew in web_reporting.live_results.views
