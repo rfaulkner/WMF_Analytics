5a49f0c3b73f02c6ec268954e465df6d91d68f2a
rfaulk <rfaulk@410e43ee-5b00-4399-9fea-894f6e993c6a>
2011-11-23 20:36:35 +0000

    mingle analytics #251 -- Add view to produce JSON object string for Central Notice stats
    Re-working the functionality to take only a campaign and give back all banners on that campaign with click rates

--- web_reporting/live_results/urls.py
+++ web_reporting/live_results/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     (r'^$', projSet.__web_app_module__ + '.live_results.views.index'),
     (r'^long_term_trends$', projSet.__web_app_module__ + '.live_results.views.long_term_trends'),
     (r'^impressions$', projSet.__web_app_module__ + '.live_results.views.impression_list'),
-    (r'^(?P<utm_campaign>[a-zA-Z0-9_]+)/(?P<banner>[a-zA-Z0-9_]+)$', projSet.__web_app_module__ + '.live_results.views.json_out'),
+    (r'^cmpgn_(?P<utm_campaign>[a-zA-Z0-9_]+)$', projSet.__web_app_module__ + '.live_results.views.json_out'),
 )
--- web_reporting/live_results/views.py
+++ web_reporting/live_results/views.py
@@ -275,18 +275,64 @@ def impression_list(request):
 
 
 """
-    Produces 
+    Produces a JSON object for a given campaign containing click rates for each banner running on that campaign
+    
+    The first query gets all of the banners and correspoding views on the corresponding campaign.  The second query gets impression counts for each banner
+      
 """
-def json_out(request, utm_campaign, banner):    
+def json_out(request, utm_campaign):    
     
+    utm_campaign = MySQLdb._mysql.escape_string(str(utm_campaign))
+                                 
     dl = DL.DataLoader(db='db1025')
+    lptl = DL.LandingPageTableLoader(db='db1008')
+        
+    start_time = lptl.get_earliest_campaign_view(utm_campaign)
+    end_time = lptl.get_latest_campaign_view(utm_campaign)
 
-    sql = "select round(views / impressions, 6) from (select sum(counts) as impressions from banner_impressions where utm_source = '%s') as bi, " + \
-    "(select count(*) as views from landing_page_requests where utm_source = '%s' and utm_campaign = '%s') as lp" 
-    sql = sql % (banner, banner, utm_campaign)
+    """ 
+        Get the views from the given campaign for each banner 
+        =====================================================
+    """    
     
-    results = dl.execute_SQL(sql)
+    logging.info('Determining views for campaign %s' % utm_campaign)
+    sql = "select utm_source, count(*) as views from landing_page_requests where utm_campaign = '%s' and request_time >= %s and request_time <= %s group by 1" % (utm_campaign, start_time, end_time)
+    results = dl.execute_SQL(str(sql))
     
-    json = '{"click_rate": %s}' % results[0][0]
+    """ builf the condition string for banners to be used in SQL to retrieve impressions"""
+    views = dict()
+    banner_str = ''
+    for row in results: 
+        views[str(row[0])] = int(row[1])
+        banner_str_piece = "utm_source = '%s' or " % row[0]
+        banner_str = banner_str + banner_str_piece
+    banner_str = banner_str[:-4]
+
+
+
+    """ 
+        Get the impressions from the given campaign for each banner 
+        ===========================================================
+    """        
     
+    logging.info('Determining impressions for campaign %s' % utm_campaign)    
+    sql = "select utm_source, sum(counts) from banner_impressions where (%s) and on_minute >= '%s' and on_minute <= '%s' group by 1" % (banner_str, start_time, end_time)    
+    results = dl.execute_SQL(str(sql))
+    
+    """ Build JSON, compute click rates """
+    click_rate = dict()
+    json = '{ '
+    err_str = ''
+    for row in results: 
+        try:
+            utm_source = row[0]
+            click_rate = float(views[utm_source]) / float(int(row[1]))
+            item = '"%s" : %s , ' % (utm_source, click_rate)
+            json = json + item 
+        
+        except:
+            err_str = err_str + utm_source + ' '
+    
+    json = json[:-2] + '}'
+        
     return render_to_response('live_results/json_out.html', {'html' : json}, context_instance=RequestContext(request))
